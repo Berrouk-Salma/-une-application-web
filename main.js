4fcc1fb958d8a247d6b35256d94bbc8d14c518e8
Berrouk-Salma <salmaberrouk4@gmail>
2024-12-14 18:47:24 +0100

    added the event by btn

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
+
 const popup_overlay=document.getElementById('popupOverlay');
 const openpop=document.getElementById('openpup');
+const close=document.getElementById('closePopup')
+popup_overlay.style.display='none';
+openpop.addEventListener('click',
+function openpop(){
+     popup_overlay.style.display='block';
+
+    
+})
+
+closePopup.addEventListener('click',
+    function closePopup() {
+        popup_overlay.style.display='none';
+        
+    })
+
+
+
